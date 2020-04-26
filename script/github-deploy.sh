2895ce44f3a5fe90c086d62cb65ed70ab0708ab8
Sheng Zhang <zhangsheng.zero@gmail.com>
2020-04-26 16:37:09 -0700

    Update.

--- script/github-deploy.sh
+++ script/github-deploy.sh
@@ -4,8 +4,8 @@ set -e
 
 bundle exec jekyll build
 chmod -R 755 _site
-cp -r _site/* github-site/
-pushd github-site
+cp -r _site/* ../github-site/
+pushd ../github-site
 git add *
 git commit -m "Update."
 git push -u origin master
